e5a7e33d25f003f85fe56c44fef6233ece08fb0c
VignanReddyNaini <vignanreddy148@gmail.com>
2025-04-23 13:27:47 +0530

    Update server.py 1

--- server.py
+++ server.py
@@ -10,10 +10,8 @@ import os
 app = Flask(__name__)
 #CORS(app, resources={r"/*": {"origins": "https://talksy-frontend.vercel.app"}}, supports_credentials=True)
 CORS(app, origins=[
-    "https://talksy-frontend.vercel.app",
     "https://talksy-frontend-new.onrender.com"
 ])
-
 
 @app.after_request
 def add_cors_headers(response):
